76992c56ef8b51b9a30c9da47b026cfedf9b7642
Scala Steward <me@scala-steward.org>
2020-02-08 02:16:39 +0100

    Update circe-generic, circe-parser to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val BcryptVersion = "0.4"
   val BetterMonadicForVersion = "0.3.1"
   val BitpeaceVersion = "0.4.3"
-  val CirceVersion = "0.12.3"
+  val CirceVersion = "0.13.0"
   val DoobieVersion = "0.8.8"
   val EmilVersion = "0.2.0"
   val FastparseVersion = "2.1.3"
