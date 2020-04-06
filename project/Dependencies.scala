b86fb4042ca3e002abe6a4c9bce67b9acfa0a2be
Scala Steward <me@scala-steward.org>
2020-04-06 20:11:55 +0200

    Update flyway-core to 6.3.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   val EmilVersion = "0.4.0"
   val FastparseVersion = "2.1.3"
   val FlexmarkVersion = "0.61.0"
-  val FlywayVersion = "6.3.2"
+  val FlywayVersion = "6.3.3"
   val Fs2Version = "2.3.0"
   val H2Version = "1.4.200"
   val Http4sVersion = "0.21.3"
