69010429986442522eb20fc6ddd10f96deb8f8e7
Scala Steward <me@scala-steward.org>
2020-05-05 08:10:57 +0200

    Update sqlite-jdbc to 3.31.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
   val PostgresVersion = "42.2.12"
   val PureConfigVersion = "0.12.3"
   val Slf4jVersion = "1.7.30"
-  val SqliteVersion = "3.30.1"
+  val SqliteVersion = "3.31.1"
   val StanfordNlpVersion = "3.9.2"
   val TikaVersion = "1.24.1"
   val YamuscaVersion = "0.6.1"
