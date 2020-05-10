2c2d5999c2c7ff52564b794bec72f3e432d601e2
Scala Steward <me@scala-steward.org>
2020-05-10 13:36:44 +0200

    Update yamusca-core to 0.6.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
   val Slf4jVersion = "1.7.30"
   val StanfordNlpVersion = "3.9.2"
   val TikaVersion = "1.24.1"
-  val YamuscaVersion = "0.6.1"
+  val YamuscaVersion = "0.6.2"
   val SwaggerUIVersion = "3.25.2"
   val SemanticUIVersion = "2.4.1"
   val TwelveMonkeysVersion = "3.5"
