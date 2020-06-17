41964027d1d7ba57e5970dbcfcf4c607ed02a226
Eike Kettner <eike.kettner@posteo.de>
2020-06-17 22:28:04 +0200

    Update docker files

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -5,11 +5,14 @@ set -e
 # Update the versions in joex.dockerfile and restserver.dockerfile,
 # docker-compose.yml and joex/entrypoint.sh; update versions here
 
-docker build -t eikek0/docspell:joex-0.6.0 -f joex.dockerfile  .
-docker build -t eikek0/docspell:restserver-0.6.0 -f restserver.dockerfile  .
+docker build -t eikek0/docspell:joex-0.7.0 -f joex.dockerfile  .
+docker build -t eikek0/docspell:restserver-0.7.0 -f restserver.dockerfile  .
+docker build -t eikek0/docspell:consumedir-0.7.0 -f consumedir.dockerfile .
 
-docker tag eikek0/docspell:restserver-0.6.0 eikek0/docspell:restserver-latest
-docker tag eikek0/docspell:joex-0.6.0 eikek0/docspell:joex-latest
+docker tag eikek0/docspell:restserver-0.7.0 eikek0/docspell:restserver-latest
+docker tag eikek0/docspell:joex-0.7.0 eikek0/docspell:joex-latest
+docker tag eikek0/docspell:consumedir-0.7.0 eikek0/docspell:consumedir-latest
+
 
 
 # test with docker-compose up
--- docker/joex/entrypoint.sh
+++ docker/joex/entrypoint.sh
@@ -3,4 +3,4 @@
 echo "Starting unoconv listener"
 unoconv -l &
 
-/opt/docspell-joex-0.6.0/bin/docspell-joex "$@"
+/opt/docspell-joex-0.7.0/bin/docspell-joex "$@"
