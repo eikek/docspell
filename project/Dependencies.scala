27ee42a72f4087a7bc70ed785b193a24e044363b
Scala Steward <me@scala-steward.org>
2020-07-23 14:21:04 +0200

    Update mariadb-java-client to 2.6.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   val KindProjectorVersion    = "0.10.3"
   val Log4sVersion            = "1.8.2"
   val LogbackVersion          = "1.2.3"
-  val MariaDbVersion          = "2.6.1"
+  val MariaDbVersion          = "2.6.2"
   val MiniTestVersion         = "2.8.2"
   val OrganizeImportsVersion  = "0.4.0"
   val PdfboxVersion           = "2.0.20"
