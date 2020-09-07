eb11b33028b19c3a9d7a6af7a489aabd35a5ee9b
Eike Kettner <eike.kettner@posteo.de>
2020-09-07 14:45:29 +0200

    Fix mariadb changsets

--- modules/store/src/main/resources/db/migration/mariadb/V1.9.1__classifier.sql
+++ modules/store/src/main/resources/db/migration/mariadb/V1.9.1__classifier.sql
@@ -3,7 +3,9 @@ CREATE TABLE `classifier_setting` (
   `enabled` boolean not null,
   `schedule` varchar(254) not null,
   `category` varchar(254) not null,
+  `item_count` int not null,
   `file_id` varchar(254),
   `created` timestamp not null,
-  foreign key (`cid`) references `collective`(`cid`)
+  foreign key (`cid`) references `collective`(`cid`),
+  foreign key (`file_id`) references `filemeta`(`id`)
 );
