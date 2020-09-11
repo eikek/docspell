9b41ffe00847c89c368d60cc8ce93ff9ae6735a7
Eike Kettner <eike.kettner@posteo.de>
2020-09-11 23:11:43 +0200

    Fix zola version to 0.11.0
    The newest version breaks inclusion of files in markdown.

--- website/shell.nix
+++ website/shell.nix
@@ -1,6 +1,6 @@
 let
   nixpkgsUnstable = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/92e990a8d6bc35f1089c76dd8ba68b78da90da59.tar.gz";
   };
   pkgsUnstable = import nixpkgsUnstable { };
 in
