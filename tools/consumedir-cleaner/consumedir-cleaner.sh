5f32ca4cdce5faf0833f1203e17368036a7b3984
Malte <git@jacobsen.online>
2020-10-29 06:19:20 +0100

    fixes hard-wired created date (https://github.com/eikek/docspell/pull/403#discussion_r513790807)

--- tools/consumedir-cleaner/consumedir-cleaner.sh
+++ tools/consumedir-cleaner/consumedir-cleaner.sh
@@ -146,7 +146,7 @@ do
       rm "$tmp_filepath"
     else
       created=$(echo $curl_result | jq -r ".items[0].created")
-      cur_dir="$ds_archive_path/$(date -d @$(echo "(1603892183000+500)/1000" | bc) +%Y-%m
+      cur_dir="$ds_archive_path/$(date -d @$(echo "($created+500)/1000" | bc) +%Y-%m
 )"
       echo "... moving to archive by month added ('$cur_dir')"
       mkdir -p "$cur_dir"
