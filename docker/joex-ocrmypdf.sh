5dbd35060ae077e7adcfb75a6f7d91496cfa3abd
totti4ever <49901208+totti4ever@users.noreply.github.com>
2020-10-31 19:35:35 +0100

    Fix ocrmypdf containers not being removed after a run
    before they would kind of lying around and pile up after a couple of processed items

--- docker/joex-ocrmypdf.sh
+++ docker/joex-ocrmypdf.sh
@@ -3,4 +3,4 @@
 if [ ! "$1" == "--version" ]; then
   echo "Using docker image for ocrmypdf (Version: $OCRMYPDF_VERSION)"
 fi
-docker run -v '/tmp/docspell-convert:/tmp/docspell-convert' -e "TZ=$TZ" jbarlow83/ocrmypdf:$OCRMYPDF_VERSION $@
+docker run --rm -v '/tmp/docspell-convert:/tmp/docspell-convert' -e "TZ=$TZ" jbarlow83/ocrmypdf:$OCRMYPDF_VERSION $@
