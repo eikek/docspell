5461321e59edf7455c04e2816943a2479298d49c
Eike Kettner <eike.kettner@posteo.de>
2021-02-14 02:20:01 +0100

    Remove unused styles and prevent postcss from purging relevant ones

--- postcss.config.js
+++ postcss.config.js
@@ -16,6 +16,7 @@ const prodPlugins =
           require("@fullhuman/postcss-purgecss")({
               content: [
                   "./modules/webapp/src/main/elm/**/*.elm",
+                  "./modules/webapp/src/main/styles/keep.txt",
                   "./modules/restserver/src/main/templates/*.html"
               ],
               defaultExtractor: content => content.match(/[A-Za-z0-9-_:/\.]+/g) || []
