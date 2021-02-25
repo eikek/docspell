c7d4c77e6d246148ca5835ed87cbf187f4dbd1cf
Eike Kettner <eike.kettner@posteo.de>
2021-02-26 00:34:57 +0100

    Allow more suggestions for date variants in English

--- modules/analysis/src/test/scala/docspell/analysis/date/DateFindSpec.scala
+++ modules/analysis/src/test/scala/docspell/analysis/date/DateFindSpec.scala
@@ -49,11 +49,11 @@ object DateFindSpec extends SimpleTestSuite {
       DateFind.findDates("on 11/05/2020", Language.English).toVector,
       Vector(
         NerDateLabel(
-          LocalDate.of(2020, 11, 5),
+          LocalDate.of(2020, 5, 11),
           NerLabel("11/05/2020", NerTag.Date, 3, 13)
         ),
         NerDateLabel(
-          LocalDate.of(2020, 5, 11),
+          LocalDate.of(2020, 11, 5),
           NerLabel("11/05/2020", NerTag.Date, 3, 13)
         )
       )
@@ -64,6 +64,40 @@ object DateFindSpec extends SimpleTestSuite {
         NerDateLabel(
           LocalDate.of(2020, 5, 21),
           NerLabel("21/05/2020", NerTag.Date, 3, 13)
+        )
+      )
+    )
+  }
+
+  test("more english variants") {
+    assertEquals(
+      DateFind.findDates("on 26/01/15", Language.English).toVector,
+      Vector(
+        NerDateLabel(
+          LocalDate.of(2015, 1, 26),
+          NerLabel("26/01/15", NerTag.Date, 3, 11)
+        ),
+        NerDateLabel(
+          LocalDate.of(2026, 1, 15),
+          NerLabel("26/01/15", NerTag.Date, 3, 11)
+        )
+      )
+    )
+
+    assertEquals(
+      DateFind.findDates("on 10/09/11", Language.English).toVector,
+      Vector(
+        NerDateLabel(
+          LocalDate.of(2011, 9, 10),
+          NerLabel("10/09/11", NerTag.Date, 3, 11)
+        ),
+        NerDateLabel(
+          LocalDate.of(2011, 10, 9),
+          NerLabel("10/09/11", NerTag.Date, 3, 11)
+        ),
+        NerDateLabel(
+          LocalDate.of(2010, 9, 11),
+          NerLabel("10/09/11", NerTag.Date, 3, 11)
         )
       )
     )
