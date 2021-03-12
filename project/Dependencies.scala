6ecbd66d8cfb90ebe3c75ef4c530fe359c2bf5f5
Scala Steward <me@scala-steward.org>
2021-03-12 14:16:45 +0100

    Update calev-circe, calev-core, calev-fs2 to 0.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val BcryptVersion           = "0.4"
   val BetterMonadicForVersion = "0.3.1"
   val BitpeaceVersion         = "0.7.0"
-  val CalevVersion            = "0.4.1"
+  val CalevVersion            = "0.4.2"
   val CatsParseVersion        = "0.3.1"
   val CirceVersion            = "0.13.0"
   val ClipboardJsVersion      = "2.0.6"
