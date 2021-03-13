177488817ddffd9a03c6920ac8f905a670da3967
Eike Kettner <eike.kettner@posteo.de>
2021-03-13 16:35:13 +0100

    Fix h2 migration
    Using java source code obviously requires `javac` during migration.

--- modules/store/src/main/resources/db/migration/h2/V1.21.0__cast_function.sql
+++ modules/store/src/main/resources/db/migration/h2/V1.21.0__cast_function.sql
@@ -1,12 +1,2 @@
 DROP ALIAS IF EXISTS CAST_TO_NUMERIC;
-CREATE ALIAS CAST_TO_NUMERIC AS '
-import java.text.*;
-import java.math.*;
-@CODE
-BigDecimal castToNumeric(String s) throws Exception {
-  try { return new BigDecimal(s); }
-  catch (Exception e) {
-    return null;
-  }
-}
-'
+CREATE ALIAS CAST_TO_NUMERIC FOR "docspell.store.impl.h2.CastNumericFun.castToNumeric";
---
+++ modules/store/src/main/scala/docspell/store/impl/h2/CastNumericFun.scala
@@ -0,0 +1,12 @@
+package docspell.store.impl.h2
+
+import java.math.BigDecimal
+
+/** This is used from within the H2 database! */
+object CastNumericFun {
+  def castToNumeric(str: String): BigDecimal =
+    try new BigDecimal(str)
+    catch {
+      case _: Throwable => null
+    }
+}
