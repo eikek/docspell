f91f4340b866126f3e8fa2636cd08a5b2dcf07fe
Eike Kettner <eike.kettner@posteo.de>
2021-04-11 20:38:00 +0200

    Fix scanmailbox form layout

--- modules/webapp/src/main/elm/Comp/ScanMailboxForm.elm
+++ modules/webapp/src/main/elm/Comp/ScanMailboxForm.elm
@@ -883,22 +883,6 @@ viewGeneral2 texts settings model =
             [ text texts.mailbox
             , B.inputRequired
             ]
-        , div [ class "mb-4" ]
-            [ label [ class S.inputLabel ]
-                [ text texts.summary
-                ]
-            , input
-                [ type_ "text"
-                , onInput SetSummary
-                , class S.textInput
-                , Maybe.withDefault "" model.summary
-                    |> value
-                ]
-                []
-            , span [ class "opacity-50 text-sm" ]
-                [ text texts.summaryInfo
-                ]
-            ]
         , Html.map ConnMsg
             (Comp.Dropdown.view2
                 connectionCfg
@@ -907,6 +891,22 @@ viewGeneral2 texts settings model =
             )
         , span [ class "opacity-50 text-sm" ]
             [ text texts.connectionInfo
+            ]
+        ]
+    , div [ class "mb-4" ]
+        [ label [ class S.inputLabel ]
+            [ text texts.summary
+            ]
+        , input
+            [ type_ "text"
+            , onInput SetSummary
+            , class S.textInput
+            , Maybe.withDefault "" model.summary
+                |> value
+            ]
+            []
+        , span [ class "opacity-50 text-sm" ]
+            [ text texts.summaryInfo
             ]
         ]
     ]
@@ -924,7 +924,7 @@ viewProcessing2 texts model =
                 model.folders
                 model.foldersModel
             )
-        , span [ class "opacity-50 text-sm mt-1" ]
+        , span [ class "opacity-50 text-sm" ]
             [ text texts.foldersInfo
             ]
         ]
