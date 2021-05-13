1bc475d3e0fd34d09e5596fbbb2c7779390aa7e0
Scala Steward <me@scala-steward.org>
2021-05-13 21:01:54 +0200

    Update munit, munit-scalacheck to 0.7.26

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
   val LogbackVersion          = "1.2.3"
   val MariaDbVersion          = "2.7.3"
   val MiniTestVersion         = "2.9.3"
-  val MUnitVersion            = "0.7.25"
+  val MUnitVersion            = "0.7.26"
   val OrganizeImportsVersion  = "0.5.0"
   val PdfboxVersion           = "2.0.23"
   val PoiVersion              = "4.1.2"
