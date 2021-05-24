25788a0b23417ee981c52477228e20a09dcd8fb4
Eike Kettner <eike.kettner@posteo.de>
2021-05-25 00:06:13 +0200

    Add routes for storing/retrieving client settings

---
+++ modules/store/src/main/resources/db/migration/postgresql/V1.23.0__clientsettings.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "client_settings" (
+  "id" varchar(254) not null primary key,
+  "client_id" varchar(254) not null,
+  "user_id" varchar(254) not null,
+  "settings_data" text not null,
+  "created" timestamp not null,
+  "updated" timestamp not null,
+  foreign key ("user_id") references "user_"("uid") on delete cascade,
+  unique ("client_id", "user_id")
+);
