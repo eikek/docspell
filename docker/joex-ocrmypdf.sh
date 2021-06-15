6984df42de7b37546c61f9615131f106d7d3ebd8
eikek <eike.kettner@posteo.de>
2021-06-16 00:31:21 +0200

    Removing old docker setup

--- docker/joex-ocrmypdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [ ! "$1" == "--version" ]; then
-  echo "Using docker image for ocrmypdf (Version: $OCRMYPDF_VERSION)"
-fi
-docker run --rm -v '/tmp/docspell-convert:/tmp/docspell-convert' -e "TZ=$TZ" jbarlow83/ocrmypdf:$OCRMYPDF_VERSION $@
--- docker/restserver-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/opt/docspell-restserver/bin/docspell-restserver $@
