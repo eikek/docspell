debdf8e7442f8d2bcb516eba5e02087f141ff6e5
Scala Steward <me@scala-steward.org>
2021-07-25 12:17:25 +0200

    Update bitpeace-core to 0.9.0-M2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
 
   val BcryptVersion           = "0.4"
   val BetterMonadicForVersion = "0.3.1"
-  val BitpeaceVersion         = "0.9.0-M1"
+  val BitpeaceVersion         = "0.9.0-M2"
   val CalevVersion            = "0.5.4"
   val CatsParseVersion        = "0.3.4"
   val CirceVersion            = "0.14.1"
