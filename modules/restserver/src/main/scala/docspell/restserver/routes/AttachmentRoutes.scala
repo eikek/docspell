08ce6b5256988eba82751a357553f15e0d495d48
eikek <eike.kettner@posteo.de>
2021-08-13 00:58:13 +0200

    Fix path to viewerjs for pdf view

--- modules/restserver/src/main/scala/docspell/restserver/routes/AttachmentRoutes.scala
+++ modules/restserver/src/main/scala/docspell/restserver/routes/AttachmentRoutes.scala
@@ -162,7 +162,7 @@ object AttachmentRoutes {
         // this route exists to provide a stable url
         // it redirects currently to viewerjs
         val attachUrl = s"/api/v1/sec/attachment/${id.id}"
-        val path      = s"/app/assets${Webjars.viewerjs}/ViewerJS/index.html#$attachUrl"
+        val path      = s"/app/assets${Webjars.viewerjs}/index.html#$attachUrl"
         SeeOther(Location(Uri(path = Uri.Path.unsafeFromString(path))))
 
       case GET -> Root / Ident(id) / "meta" =>
