9785db068396ffad5fec08571b2225c5f2c3b76c
eikek <eike.kettner@posteo.de>
2021-09-21 22:32:47 +0200

    Change license header of all files

--- modules/joex/src/main/scala/docspell/joex/preview/AllPreviewsTask.scala
+++ modules/joex/src/main/scala/docspell/joex/preview/AllPreviewsTask.scala
@@ -1,7 +1,7 @@
 /*
- * Copyright 2020 Docspell Contributors
+ * Copyright 2020 Eike K. & Contributors
  *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 package docspell.joex.preview
