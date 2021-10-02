5265c212c7cdcc3734145b14ccb990c16166dbbf
Scala Steward <me@scala-steward.org>
2021-10-03 00:52:45 +0200

    Update pureconfig to 0.17.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
   val PdfboxVersion = "2.0.24"
   val PoiVersion = "4.1.2"
   val PostgresVersion = "42.2.24"
-  val PureConfigVersion = "0.16.0"
+  val PureConfigVersion = "0.17.0"
   val ScalaJavaTimeVersion = "2.3.0"
   val ScodecBitsVersion = "1.1.29"
   val Slf4jVersion = "1.7.32"
