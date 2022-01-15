3ec47349dc9e2d5e39c4aa4f8e52f496f60e2cad
Scala Steward <me@scala-steward.org>
2022-01-15 02:26:11 +0100

    Update calev-circe, calev-core to 0.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val BcryptVersion = "0.4"
   val BetterMonadicForVersion = "0.3.1"
   val BinnyVersion = "0.2.1"
-  val CalevVersion = "0.6.0"
+  val CalevVersion = "0.6.1"
   val CatsParseVersion = "0.3.6"
   val CirceVersion = "0.14.1"
   val ClipboardJsVersion = "2.0.6"
