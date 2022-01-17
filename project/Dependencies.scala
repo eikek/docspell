37aa0024ea653b382395b19852d1c0664e17b1d8
Scala Steward <me@scala-steward.org>
2022-01-17 21:16:36 +0100

    Update h2 to 2.1.210

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val FlywayVersion = "8.4.1"
   val Fs2Version = "3.2.4"
   val Fs2CronVersion = "0.7.1"
-  val H2Version = "1.4.200"
+  val H2Version = "2.1.210"
   val Http4sVersion = "0.23.7"
   val Icu4jVersion = "70.1"
   val JavaOtpVersion = "0.3.1"
