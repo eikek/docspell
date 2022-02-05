b8bdc7d95e966ee53c0c790a71edf1aa7d0204a7
Scala Steward <me@scala-steward.org>
2022-02-05 22:49:18 +0100

    Update swagger-ui to 4.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,7 +44,7 @@ object Dependencies {
   val StanfordNlpVersion = "4.4.0"
   val TikaVersion = "2.2.1"
   val YamuscaVersion = "0.8.2"
-  val SwaggerUIVersion = "4.4.1-1"
+  val SwaggerUIVersion = "4.5.0"
   val TestContainerVersion = "0.40.0"
   val TwelveMonkeysVersion = "3.8.1"
   val JQueryVersion = "3.5.1"
