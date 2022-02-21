79d29229ae9f8aa2f122b8dd2896763780fc3c9c
eikek <eike.kettner@posteo.de>
2022-02-21 22:52:39 +0100

    Add more breakpoints and increase card column count
    Refs: #1401

--- modules/webapp/tailwind.config.js
+++ modules/webapp/tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
                "./src/main/styles/keep.txt",
                "../restserver/src/main/templates/*.html"
              ],
+    theme: {
+        extend: {
+            screens: {
+                '3xl': '1792px',
+                '4xl': '2048px',
+                '5xl': '2560px',
+                '6xl': '3072px',
+                '7xl': '3584px'
+            }
+        }
+    },
     variants: {
         extend: {
             backgroundOpacity: ['dark']
