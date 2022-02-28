0ce76ebebd5554de07c2992d84b2475d94b738ee
Scala Steward <me@scala-steward.org>
2022-02-28 07:20:10 +0100

    Update emil-common, emil-doobie, ... to 0.11.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val CirceVersion = "0.14.1"
   val ClipboardJsVersion = "2.0.6"
   val DoobieVersion = "1.0.0-RC2"
-  val EmilVersion = "0.11.1"
+  val EmilVersion = "0.11.2"
   val FlexmarkVersion = "0.64.0"
   val FlywayVersion = "8.5.1"
   val Fs2Version = "3.2.5"
