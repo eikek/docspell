32bd4e82daa2a604302165acaa6e708d0220580e
Scala Steward <me@scala-steward.org>
2022-03-04 16:13:43 +0100

    Update flyway-core, flyway-mysql to 8.5.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val DoobieVersion = "1.0.0-RC2"
   val EmilVersion = "0.11.2"
   val FlexmarkVersion = "0.64.0"
-  val FlywayVersion = "8.5.1"
+  val FlywayVersion = "8.5.2"
   val Fs2Version = "3.2.5"
   val Fs2CronVersion = "0.7.1"
   val H2Version = "1.4.200"
