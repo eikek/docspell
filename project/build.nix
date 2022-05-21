eb6a36f900bfc3b9ecc8595922f323d5959b783c
eikek <eike.kettner@posteo.de>
2022-05-21 23:33:22 +0200

    Update build.nix

--- project/build.nix
+++ project/build.nix
@@ -1,19 +1,19 @@
 let
   nixpkgs = builtins.fetchTarball {
-    ## need fix to zola-0.11.0 for now
-    url = "https://github.com/NixOS/nixpkgs/archive/92e990a8d6bc35f1089c76dd8ba68b78da90da59.tar.gz";
+    #url = "https://github.com/NixOS/nixpkgs/archive/92e990a8d6bc35f1089c76dd8ba68b78da90da59.tar.gz";
+    url =  "channel:nixos-21.11";
   };
   pkgs = import nixpkgs { };
   initScript = pkgs.writeScript "docspell-build-init" ''
      export LD_LIBRARY_PATH=
-     ${pkgs.bash}/bin/bash -c "sbt -mem 2048 -java-home ${pkgs.openjdk11}/lib/openjdk"
+     ${pkgs.bash}/bin/bash -c "sbt -mem 4096 -java-home ${pkgs.openjdk11}/lib/openjdk"
   '';
 in with pkgs;
 
 buildFHSUserEnv {
   name = "docspell-sbt";
   targetPkgs = pkgs: with pkgs; [
-    netcat jdk8 wget which zsh dpkg sbt git elmPackages.elm ncurses fakeroot mc
+    netcat jdk11 wget which dpkg sbt git elmPackages.elm ncurses fakeroot mc
     zola yarn
 
     # haskells http client needs this (to download elm packages)
