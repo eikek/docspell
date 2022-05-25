30954a46b91dd683351292d43ae56e51b99dbc25
Scala Steward <me@scala-steward.org>
2022-05-25 03:28:24 +0200

    Update http4s-circe, http4s-dsl to 0.23.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   val FlywayVersion = "8.5.11"
   val Fs2Version = "3.2.7"
   val H2Version = "1.4.200"
-  val Http4sVersion = "0.23.11"
+  val Http4sVersion = "0.23.12"
   val Icu4jVersion = "71.1"
   val JavaOtpVersion = "0.3.1"
   val JsoupVersion = "1.15.1"
