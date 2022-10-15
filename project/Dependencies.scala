bf9d8e67e9360521e0f8805af5caeb1580211da9
eikek-scala-steward <eikek-scala-steward@users.noreply.github.comb>
2022-10-15 06:27:24 +0000

    Revert commit(s) a04f3d06

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
   val StanfordNlpVersion = "4.5.1"
   val TikaVersion = "2.5.0"
   val YamuscaVersion = "0.9.0"
-  val SwaggerUIVersion = "4.14.3"
+  val SwaggerUIVersion = "4.14.2"
   val TestContainerVersion = "0.40.10"
   val TwelveMonkeysVersion = "3.8.3"
   val JQueryVersion = "3.5.1"
