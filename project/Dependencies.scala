63388c7c69051eb0882863be6341e1215cb09114
eikek-scala-steward <eikek-scala-steward@users.noreply.github.comb>
2022-11-22 06:15:19 +0000

    Update imageio-jpeg, imageio-tiff to 3.9.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -51,7 +51,7 @@ object Dependencies {
   val YamuscaVersion = "0.10.0"
   val SwaggerUIVersion = "4.15.5"
   val TestContainerVersion = "0.40.11"
-  val TwelveMonkeysVersion = "3.9.3"
+  val TwelveMonkeysVersion = "3.9.4"
   val JQueryVersion = "3.5.1"
 
   val scribe = Seq(
