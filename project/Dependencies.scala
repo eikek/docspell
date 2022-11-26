df35f64ea37321ecefb51a0c62caa14a6f4c8f94
eikek-scala-steward <eikek-scala-steward@users.noreply.github.comb>
2022-11-26 06:13:05 +0000

    Update jcl-over-slf4j to 2.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,7 +44,7 @@ object Dependencies {
   val ScalaJavaTimeVersion = "2.4.0"
   val ScodecBitsVersion = "1.1.34"
   val ScribeVersion = "3.10.5"
-  val Slf4jVersion = "2.0.4"
+  val Slf4jVersion = "2.0.5"
   val SourcecodeVersion = "0.3.0"
   val StanfordNlpVersion = "4.5.1"
   val TikaVersion = "2.6.0"
