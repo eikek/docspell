05cc896d5450bb8b85138881e94e9abbc16ff24a
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2023-04-01 06:18:10 +0000

    Update swagger-ui to 4.18.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
   val StanfordNlpVersion = "4.5.3"
   val TikaVersion = "2.7.0"
   val YamuscaVersion = "0.10.0"
-  val SwaggerUIVersion = "4.18.1"
+  val SwaggerUIVersion = "4.18.2"
   val TestContainerVersion = "0.40.14"
   val TwelveMonkeysVersion = "3.9.4"
   val JQueryVersion = "3.5.1"
