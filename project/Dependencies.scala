23f34b86110c21e438b57a8f469a980f7d499cf4
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2023-06-15 06:14:30 +0000

    Update testcontainers-scala-mariadb, ... to 0.40.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -50,7 +50,7 @@ object Dependencies {
   val TikaVersion = "2.8.0"
   val YamuscaVersion = "0.10.0"
   val SwaggerUIVersion = "4.19.1"
-  val TestContainerVersion = "0.40.14"
+  val TestContainerVersion = "0.40.16"
   val TwelveMonkeysVersion = "3.9.4"
   val JQueryVersion = "3.5.1"
 
