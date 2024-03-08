4d0e2c059935a2a90d18e3ad842b91cfe772bd2e
Scala Steward <scala_steward@virtuslab.com>
2024-03-08 15:58:13 +0000

    Update kittens to 3.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
   val JsoupVersion = "1.17.2"
   val JwtScalaVersion = "10.0.0"
   val KindProjectorVersion = "0.10.3"
-  val KittensVersion = "3.2.0"
+  val KittensVersion = "3.3.0"
   val LevigoJbig2Version = "2.0"
   val Log4sVersion = "1.10.0"
   val LogbackVersion = "1.2.10"
