788ffab63c11d6158ea552db8ccde6d6fb123755
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2024-04-20 06:24:14 +0000

    Update stanford-corenlp to 4.5.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,7 +45,7 @@ object Dependencies {
   val ScribeVersion = "3.13.3"
   val Slf4jVersion = "2.0.13"
   val SourcecodeVersion = "0.4.1"
-  val StanfordNlpVersion = "4.5.6"
+  val StanfordNlpVersion = "4.5.7"
   val TikaVersion = "2.9.2"
   val YamuscaVersion = "0.10.0"
   val SwaggerUIVersion = "5.15.2-1"
