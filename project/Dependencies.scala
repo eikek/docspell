53874b09b2e4a737f5c7cb21363199848cdbd961
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2025-03-14 06:16:03 +0000

    Update icu4j to 77.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   val Fs2Version = "3.11.0"
   val H2Version = "2.3.232"
   val Http4sVersion = "0.23.30"
-  val Icu4jVersion = "76.1"
+  val Icu4jVersion = "77.1"
   val JavaOtpVersion = "0.4.0"
   val JsoupVersion = "1.19.1"
   val JwtScalaVersion = "10.0.4"
