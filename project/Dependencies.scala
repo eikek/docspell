38a4bc410df7cec3c0228f65747713ba63bbafc0
Scala Steward <scala_steward@virtuslab.com>
2025-07-01 17:11:02 +0000

    Update scodec-bits to 1.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
   val PostgresVersion = "42.7.7"
   val PureConfigVersion = "0.17.9"
   val ScalaJavaTimeVersion = "2.6.0"
-  val ScodecBitsVersion = "1.2.1"
+  val ScodecBitsVersion = "1.2.2"
   val ScribeVersion = "3.16.1"
   val Slf4jVersion = "2.0.17"
   val SourcecodeVersion = "0.4.2"
